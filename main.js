522877f22368e4df28e588df34dd7ffb9eeca95f
fardinkhan69 <fardinofficial69@gmail.com>
2021-01-28 13:32:33 +0600

    juratali

--- main.js
+++ main.js
@@ -10,6 +10,15 @@ plusIconCasing.addEventListener("click",function(){
     
     document.getElementById("casing_quantity").value = casing_quantity_new;
 
+
+
+    const casingPriceSingle = 59;
+    const casingPrice = document.getElementById("casingPrice").innerText;
+    const casingPriceNum = parseFloat(casingPrice);
+
+    let totalPrice = casingPriceNum + casingPriceSingle;
+    document.getElementById("casingPrice").innerText = totalPrice;
+
   })
 
 
@@ -20,4 +29,29 @@ minusIconCasing.addEventListener("click",function(){
     let casing_quantity_value = parseFloat(casing_quantity);
     let casing_quantity_minus = casing_quantity_value - 1;
     document.getElementById("casing_quantity").value = casing_quantity_minus;
+
+    if (document.getElementById("casing_quantity").value< 0 ){
+        document.getElementById("casing_quantity").value = 0;
+    }
+    
+    const casingPriceSingle = 59;
+    const casingPrice = document.getElementById("casingPrice").innerText;
+    const casingPriceNum = parseFloat(casingPrice);
+
+    let totalPrice = casingPriceNum - casingPriceSingle;
+
+    document.getElementById("casingPrice").innerText = totalPrice;
+
+
+   if(parseFloat(document.getElementById("casingPrice").innerText)<0){
+    document.getElementById("casingPrice").innerText = 0;
+   }
+
+
 })
+
+
+
+function changeSpanText(){
+
+}
